b40760ff6567cbf4fa93d60680be8a552fe814ca
AlanDAmario <vega93@live.it>
2024-07-10 16:42:49 +0200

    [upd] check

---
+++ database/migrations/2024_07_10_055928_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->tinyInteger('number_of_carriages')->default(3);
+            $table->boolean('in_time')->default(false);
+            $table->boolean('cancelled')->default(false);
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
